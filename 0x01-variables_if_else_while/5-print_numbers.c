98d8bfd716dd94df59327f730c393c924b6ebe41
hadijah10 <mohamaduhadijatu@gmail.com>
2023-04-02 13:56:38 -0700

    single numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * print singles numbers
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0; a <= 9; a++)
+	{
+		printf("%d", a);
+	}
+	printf("\n");
+	return (0);
+}
